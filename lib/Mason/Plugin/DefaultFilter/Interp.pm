f2e29849c224fc0c7ca336cc5db10dfe1da8e6fe
Pedro Melo <melo@simplicidade.org>
2012-03-06 11:03:34 +0000

    Added missing 'is' argument to 'default_filters' attribute
    Recent versions of Moose require a 'is' argument. The error message is this:
    
        Attribute (default_filters) of class Moose::Meta::Class::__ANON__::SERIAL::4
        has no associated methods (did you mean to provide an "is" argument?)

--- lib/Mason/Plugin/DefaultFilter/Interp.pm
+++ lib/Mason/Plugin/DefaultFilter/Interp.pm
@@ -4,6 +4,6 @@ use Mason::PluginRole;
 
 # VERSION
 
-has default_filters => ( isa => 'ArrayRef', default => sub { [] } );
+has default_filters => ( is => 'ro', isa => 'ArrayRef', default => sub { [] } );
 
 1;
